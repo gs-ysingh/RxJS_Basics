4d5b977d0249ad66fbb1182ec9ac57051a51fd55
Yogesh Singh <ysingh@gainsight.com>
2017-01-16 23:21:51 +0530

    added few examples

--- src/app.js
+++ src/app.js
@@ -1,4 +1,24 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('RxJS Boiler Running...');
+Rx.Observable.fromEvent(document, 'mousemove')
+    .subscribe(e => {
+        $('#timer').html(e.clientX + ', ' +e.clientY);
+    });
+
+var getData = function () {
+    return $.ajax({
+        url: "https://api.github.com/users/gs-ysingh",
+        dataType: "jsonp"
+    }).promise();
+}
+
+Rx.Observable.fromPromise(getData())
+    .subscribe(
+        (x) => {
+            console.log( x);
+            $('#img').attr('src', x.data.avatar_url);
+        },
+        (e) => { console.log('error' + e); },
+        () => { console.log('completed');  }
+    );
